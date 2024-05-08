b95a844b70a6036a0e7a67b19f282377e4b7e702
andy <panandy0502@gmail.com~git config --global user.email panandy0502@gmail.com>
2024-05-08 23:22:31 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Picruise</title>
+    <!--接到CSS檔案-->
+    <link rel="stylesheet" href="css/index.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Picruise</h1>
+      <!-- 新增登入和註冊按鈕結束 --> 
+    </header>    
+    <!-- 左邊設定欄位 -->
+    <aside>
+      <ul>
+        <li>
+          <img src="Allphotos/home1.png" alt="home" /><a
+            href="index.php"
+            class="button set-button"
+            >Home</a
+          >
+        </li>
+        <li>
+          <img src="Allphotos/Initiate.png" alt="create" /><a
+            href="allDiscussion.html"
+            class="button set-button"
+            >Initiate</a
+          >
+        </li>
+        <li>
+          <img src="Allphotos/Profile1.png" alt="profile" /><a
+            href="profile.html"
+            class="button set-button"
+            >Profile</a
+          >
+        </li>
+        <li>
+          <img src="Allphotos/Accounts.png" alt="notification" /><a
+            href="accoutSettings.html"
+            class="button set-button"
+            >Setting</a
+          >
+        </li>
+        <li>
+          <img src="Allphotos/login.png" alt="Forum" /><a
+            href="loginpage.html"
+            class="button set-button"
+            >Log in/out</a
+          >
+        </li>
+        <li>
+          <img src="Allphotos/SignUp.png" alt="Forum" /><a
+            href="singUpPage.html"
+            class="button set-button"
+            >Sign Up</a
+          >
+        </li>
+        <!-- 添加更多主題 -->
+      </ul>
+    </aside>
+    <!-- 左邊設定欄位結束 -->
+    <!-- 主題區 -->
+    <!-- 1 -->
+    <div class="card1">
+      <div class="card">
+        <img src="photo.jpg" class="card-img-top" alt="photo.jpg" />
+        <div class="card-body">
+          <h5 class="card-title">The Cat Within My Gaze...</h5>
+          <a href="discussion.html" class="joinbutton">Join</a>
+        </div>
+      </div>
+    </div>
+    <!-- 2 -->
+    <div class="card2">
+      <div class="card">
+        <img src="photo.jpg" class="card-img-top" alt="photo.jpg" />
+        <div class="card-body">
+          <h5 class="card-title">Title 2</h5>
+          <a href="discussion.html" class="joinbutton">Join</a>
+        </div>
+      </div>
+    </div>
+    <!-- 3 -->
+    <div class="card3">
+      <div class="card">
+        <img src="photo.jpg" class="card-img-top" alt="photo.jpg" />
+        <div class="card-body">
+          <h5 class="card-title">Title 3</h5>
+          <a href="discussion.html" class="joinbutton">Join</a>
+        </div>
+      </div>
+    </div>
+    <!-- 4 -->
+    <div class="card4">
+      <div class="card">
+        <img src="photo.jpg" class="card-img-top" alt="photo.jpg" />
+        <div class="card-body">
+          <h5 class="card-title">Title 4</h5>
+          <a href="discussion.html" class="joinbutton">Join</a>
+        </div>
+      </div>
+    </div>
+    <!-- 5 -->
+    <div class="card5">
+      <div class="card">
+        <img src="photo.jpg" class="card-img-top" alt="photo.jpg" />
+        <div class="card-body">
+          <h5 class="card-title">Title 5</h5>
+          <a href="discussion.html" class="joinbutton">Join</a>
+        </div>
+      </div>
+    </div>
+    <!-- 6 -->
+    <div class="card6">
+      <div class="card">
+        <img src="photo.jpg" class="card-img-top" alt="photo.jpg" />
+        <div class="card-body">
+          <h5 class="card-title">Title 6</h5>
+          <a href="discussion.html" class="joinbutton">Join</a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
