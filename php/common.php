529538dd2ad08fc2c70cbeeee8cb6e161376808b
a111403055 <richard930820@gmail.com>
2024-05-25 19:07:23 +0800

    5/25(1)

--- php/common.php
+++ php/common.php
@@ -1,18 +1,25 @@
 <?php
 
+/*$func_name = @$_POST['func_name'];
+$tb_name_post = @$_POST['tb_name_post'];
+$row_name_post = @$_POST['row_name_post'];
+$where_post = @$_POST['where_post'];
+$key_post = @$_POST['key_post'];
 
+if ($func_name == "updateData") {
+    $val_post = @$_POST['val_post'];
+    updateData($tb_name_post, $row_name_post, $val_post, $where_post, $key_post);
+}
 
-
-
+*/
 
 function checkHasData($tableName, $rowName, $_where, $_key)
 {
-    require("../PHP/connectDB.php");
+    require("../php/connectDB.php");
     if (!(isset($_SESSION))) {
         session_start();
     };
     //mysqli_query($conn, 'SET NAMES utf8');
-
 
     $sql = "SELECT $rowName FROM $tableName WHERE $_where='" . $_key . "'";
     $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
--- php/login.php
+++ php/login.php
@@ -1,6 +1,6 @@
 <?php
-//登入
-require_once("../PHP/connectDB.php");
+
+require_once("../php/connectDB.php");
 $tablename = "users";
 mysqli_query($conn, 'SET NAMES utf8');
 
@@ -14,17 +14,15 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         //驗證password hash
         if (password_verify($passwd_post, @$row["password"])) {
-
             session_start();
-            $_SESSION['id'] = @$row['ID'];
-            setcookie('id', @$row['ID'], time() + 60 * 60 * 7);
-            
-            //echo "<script>alert('登入成功')</script>";
+            $_SESSION['ID'] = @$row['ID'];
+            setcookie('ID', @$row['ID'], time() + 60 * 60 * 7);
+            //echo "<script>alert('GO')</script>";
             echo "<script> location.href='../index.php'  </script>";
         }
-        else{
+        else {
             echo "<script>alert('密碼錯誤')</script>";
-            echo "<script>location.href='../index.php'</script>";
+            echo "<script>location.href='../page/login.php'</script>";
         }
     }
 }
